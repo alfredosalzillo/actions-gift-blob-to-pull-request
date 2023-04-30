83e63125a57d94fc8beae5de34d9694f56822289
alfredosalzillo <noctisbels@gmail.com>
2023-04-30 18:30:49 +0200

    chore: added eslint

--- src/blobs.ts
+++ src/blobs.ts
@@ -1,14 +1,16 @@
 import fetch from 'node-fetch';
+
+// eslint-disable-next-line import/prefer-default-export
 export const generateBlobUri = async (width: number, height: number) => {
-    const response = await fetch(`https://blobs-six.vercel.app/api/blob?width=${width}&height=${height}`, {
-        redirect: 'manual',
-    });
-    if (response.status !== 302) {
-        throw new Error(`Blob API returned ${response.status}`);
-    }
-    const location = response.headers.get('location');
-    if (!location) {
-        throw new Error(`Blob API returned no location header`);
-    }
-    return location;
-}
+  const response = await fetch(`https://blobs-six.vercel.app/api/blob?width=${width}&height=${height}`, {
+    redirect: 'manual',
+  });
+  if (response.status !== 302) {
+    throw new Error(`Blob API returned ${response.status}`);
+  }
+  const location = response.headers.get('location');
+  if (!location) {
+    throw new Error('Blob API returned no location header');
+  }
+  return location;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import * as github from '@actions/github';
 import * as core from '@actions/core';
 import outdent from 'outdent';
-import { generateBlobUri } from "./blobs";
+
+import { generateBlobUri } from './blobs';
 
 async function run() {
   try {
-    const context = github.context;
+    const { context } = github;
     if (!context.payload.pull_request) {
       core.setFailed('No pull request provided');
       return;
@@ -18,20 +19,22 @@ async function run() {
     const blobWidth = Number(core.getInput('blob_width'));
     const blobHeight = Number(core.getInput('blob_height'));
     const tag = core.getInput('tag');
-    const tagPattern = `<!-- alfredosalzillo/actions-gift-blob-to-pull-request "${tag}" -->`
+    const tagPattern = `<!-- alfredosalzillo/actions-gift-blob-to-pull-request "${tag}" -->`;
     const issueNumber = context.payload.pull_request.number;
     const octokit = github.getOctokit(githubToken);
     const haveAlreadyBeenCommented = async () => {
-      for await (const { data: comments } of octokit.paginate.iterator(octokit.rest.issues.listComments, {
-        ...context.repo,
-        issue_number: issueNumber,
-      })) {
+      // eslint-disable-next-line no-restricted-syntax
+      for await (const { data: comments } of octokit.paginate
+        .iterator(octokit.rest.issues.listComments, {
+          ...context.repo,
+          issue_number: issueNumber,
+        })) {
         if (comments.some((comment) => comment?.body?.includes(tagPattern))) {
-          return true
+          return true;
         }
       }
-      return false
-    }
+      return false;
+    };
     if (await haveAlreadyBeenCommented()) {
       core.info(`PR #${issueNumber} has already been commented`);
       return;
@@ -39,12 +42,12 @@ async function run() {
     const pr = await octokit.rest.issues.get({
       ...context.repo,
       issue_number: issueNumber,
-    })
+    });
     if (!pr.data.user) {
       core.setFailed(`PR #${issueNumber} has no user`);
       return;
     }
-    const blobUri = await generateBlobUri(blobWidth, blobHeight)
+    const blobUri = await generateBlobUri(blobWidth, blobHeight);
     const opener = pr.data.user.login;
     const body = outdent`
       ${tagPattern}
@@ -52,7 +55,7 @@ async function run() {
       
       A special blob has been generated for you:
       [![Blob](${blobUri})](${blobUri})
-    `
+    `;
     await octokit.rest.issues.createComment({
       ...context.repo,
       issue_number: issueNumber,
